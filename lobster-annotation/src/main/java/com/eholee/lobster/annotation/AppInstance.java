480ea8c783504881847bce941b6e1b30cb0c751f
jeffer <jeffer7150@163.com>
2020-12-16 23:45:09 +0800

    更新注解库

--- lobster-annotation/src/main/java/com/eholee/lobster/annotation/AppInstance.java
+++ lobster-annotation/src/main/java/com/eholee/lobster/annotation/AppInstance.java
@@ -1,5 +1,6 @@
 package com.eholee.lobster.annotation;
 
+
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
@@ -7,10 +8,10 @@ import java.lang.annotation.Target;
 
 /**
  * Author:Jeffer
- * Time:2020/12/16  11:09 PM  Wednesday
- * Description: 壳子（主工程）Application注解
+ * Time:2020/12/16  11:13 PM  Wednesday
+ * Description: 子模块Application实例注解
  */
 @Retention(RetentionPolicy.CLASS)
-@Target({ElementType.TYPE})
-public @interface ShellApp {
+@Target({ElementType.FIELD})
+public @interface AppInstance {
 }
--- lobster-annotation/src/main/java/com/eholee/lobster/annotation/ModuleApp.java
+++ lobster-annotation/src/main/java/com/eholee/lobster/annotation/ModuleApp.java
@@ -1,5 +1,6 @@
 package com.eholee.lobster.annotation;
 
+
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
@@ -8,9 +9,10 @@ import java.lang.annotation.Target;
 /**
  * Author:Jeffer
  * Time:2020/12/16  11:09 PM  Wednesday
- * Description: 壳子（主工程）Application注解
+ * Description: 子模块Application注解
  */
 @Retention(RetentionPolicy.CLASS)
 @Target({ElementType.TYPE})
-public @interface ShellApp {
+public @interface ModuleApp {
+    int priority() default 100;
 }
